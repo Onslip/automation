8cc5366c182801dabee7977230eba303e87c1878
Martin Blom <martin.blom@onslip.com>
2022-03-29 18:54:41 +0200

    Added stopAndroidApplication and executeAndroidShellCommand helpers.

--- src/adb.ts
+++ src/adb.ts
@@ -25,13 +25,21 @@ export async function findAndroidDevices(): Promise<string[]> {
         .map(line => /(^[^\s]+)\s+device$/.exec(line)?.[1]!).filter(Boolean);
 }
 
+export async function executeAndroidShellCommand(device: string, command: string): Promise<string> {
+    return (await execFile('adb', [ '-s', device, 'shell', command ])).stdout;
+}
+
 export async function startAndroidActivity(device: string, activity: string, options?: { restart?: boolean, wait?: boolean }) {
-    await execFile('adb', ['-s', device, 'shell',
-        `am start ${options?.restart ? '-S' : ''} ${options?.wait ? '-W' : ''} -a android.intent.action.MAIN -n "${activity}"`]);
+    await executeAndroidShellCommand(device,
+        `am start ${options?.restart ? '-S' : ''} ${options?.wait ? '-W' : ''} -a android.intent.action.MAIN -n "${activity}"`);
+}
+
+export async function stopAndroidApplication(device: string, packageName: string) {
+    await executeAndroidShellCommand(device, `am force-stop "${packageName}"`);
 }
 
 export async function findAndroidWebViews(device: string): Promise<string[]> {
-    return (await execFile('adb', ['-s', device, 'shell', 'cat /proc/net/unix'])).stdout.split('\n')
+    return (await executeAndroidShellCommand(device, 'cat /proc/net/unix')).split('\n')
         .map(line => /@(.*devtools_remote.*)/.exec(line)?.[1]!).filter(Boolean);
 }
 
